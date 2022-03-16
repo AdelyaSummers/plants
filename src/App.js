c76a8670a2e603504ba63d7b238910b9dd9f0d63
AdelyaSummers <adelyasummers@gmail.com>
2022-03-15 23:25:09 -0400

    plants favorite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+  const [plants, setPlants] = useState(data);
+
+  const removePlants =(id) => {
+    let newPlants = plants.filter( plant => plant.id !==id);
+    setPlants(newPlants)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="container">
+        <h1> {plants.length} plants I wish for my birthday</h1>
+      </div>
+
+      {plants.map((element => {
+        const {id, description, image} = element;
+
+        return (<div key={id}>
+          <div className="container">
+            <h2>{id} - {description}</h2>
+          </div>
+
+          <div className="container">
+            <img src={image} width="400px" alt="plants"/>
+          </div>
+          <div className="container">
+            <button onClick={() => removePlants(id)}>remove</button>
+          </div>
+
+        </div>)
+      }))}
+
+        <div className="container">
+          <button onClick={() => setPlants([])}>Delete all</button>
+        </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        description: "Chinese Money Plant",
+        image:"https://images.unsplash.com/photo-1598880942562-8cf3cf728312?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    },
+    {
+        id: 2,
+        description:"Sansevieria",
+        image:"https://images.unsplash.com/photo-1642004346232-7717892cec48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+        id:3,
+        description:"African Violet",
+        image:"https://images.unsplash.com/photo-1631019650382-a0945075a592?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+        id:4,
+        description: "Maranta",
+        image:"https://images.unsplash.com/photo-1638353842736-3f40d480488b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    },
+    {
+        id:5,
+        description:"Parlor palm",
+        image:"https://images.unsplash.com/photo-1506634064465-7dab4de896ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+        id:6,
+        description:"Air plant",
+        image:"https://images.unsplash.com/photo-1602491673920-1d856a9574b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+        id:7,
+        description:"Monstera",
+        image:"https://images.unsplash.com/photo-1525498128493-380d1990a112?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
+    },
+]
